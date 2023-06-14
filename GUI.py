db677367d51a5114b5562d800e310dfc019d851c
richard <115185992+duck1939@users.noreply.github.com>
2023-06-14 15:57:28 -0400

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -3,45 +3,81 @@
 import time
 import tkinter as tk
 
-def start_timer(duration):
-    end_time = time.time() + duration
-    while time.time() < end_time:
-        remaining_time = end_time - time.time()
-        minutes, seconds = divmod(int(remaining_time), 60)
-        hours, minutes = divmod(minutes, 60)
-        timer_label['text'] = f"{hours:02d}:{minutes:02d}:{seconds:02d}"
-        root.update()
-        time.sleep(1)
-    timer_label['text'] = "TIME'S UP!"
+class Timer:
+    def __init__(self, duration):
+        self.duration = duration
+        self.end_time = None
+        self.timer_running = False
+    
+    def start(self):
+        self.end_time = time.time() + self.duration
+        self.timer_running = True
+    
+    def stop(self):
+        self.timer_running = False
+    
+    def update(self):
+        if self.timer_running:
+            remaining_time = self.end_time - time.time()
+            if remaining_time <= 0:
+                self.timer_running = False
+                return "TIME'S UP!"
+            minutes, seconds = divmod(int(remaining_time), 60)
+            hours, minutes = divmod(minutes, 60)
+            return f"{hours:02d}:{minutes:02d}:{seconds:02d}"
+        return "00:00:00"
 
+event_timer = Timer(0)
+screen_timer = Timer(0)
+# starting the timers
 def start_event_timer():
     duration = int(event_timer_entry.get())
-    start_timer(duration)
+    event_timer.duration = duration
+    event_timer.start()
 
 def start_screen_timer():
     duration = int(screen_timer_entry.get())
-    start_timer(duration)
+    screen_timer.duration = duration
+    screen_timer.start()
+# stoping the timers 
+def stop_event_timer():
+    event_timer.stop()
+
+def stop_screen_timer():
+    screen_timer.stop()
+
+def update_timers():
+    event_timer_label['text'] = event_timer.update()
+    screen_timer_label['text'] = screen_timer.update()
+    root.after(1000, update_timers)
 
 root = tk.Tk()
-root.title("Timer App")
+root.title("Timer Pro v2 App")
 
-event_timer_label = tk.Label(root, text="Event Timer (seconds):")
+event_timer_label = tk.Label(root, text="00:00:00", font=("Arial", 24), pady=10)
 event_timer_label.pack()
+
 event_timer_entry = tk.Entry(root)
 event_timer_entry.pack()
 
 event_timer_button = tk.Button(root, text="Start Event Timer", command=start_event_timer)
 event_timer_button.pack()
 
-screen_timer_label = tk.Label(root, text="Screen Timer (seconds):")
+event_timer_stop_button = tk.Button(root, text="Stop Event Timer", command=stop_event_timer)
+event_timer_stop_button.pack()
+
+screen_timer_label = tk.Label(root, text="00:00:00", font=("Arial", 24), pady=10)
 screen_timer_label.pack()
+
 screen_timer_entry = tk.Entry(root)
 screen_timer_entry.pack()
 
 screen_timer_button = tk.Button(root, text="Start Screen Timer", command=start_screen_timer)
 screen_timer_button.pack()
 
-timer_label = tk.Label(root, text="00:00:00", font=("Arial", 24), pady=10)
-timer_label.pack()
+screen_timer_stop_button = tk.Button(root, text="Stop Screen Timer", command=stop_screen_timer)
+screen_timer_stop_button.pack()
+
+update_timers()
 
 root.mainloop()
